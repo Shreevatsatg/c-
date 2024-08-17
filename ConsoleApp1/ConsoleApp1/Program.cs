39bd5f211b57d89a203d94bd3f663bc429e39889
Shreevatsatg <tgshreevatsa@gmail.com>
2024-08-17 10:40:16 +0530

    added initial files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+var num = "helloworld";
+Console.WriteLine(num);
---
+++ WindowsService1/WindowsService1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+            Console.writeline("hello world");
+        }
+    }
+}
