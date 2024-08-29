a5c0ae080feeeec3ba88ac816789c3810dfac1cc
Shreevatsatg <tgshreevatsa@gmail.com>
2024-08-29 10:21:17 +0530

    added a simple hello world program

---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("thank you for clicking me")
+
+    End Sub
+End Class
---
+++ WinFormsApp3/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.PictureBox1.Image = My.Resources.pexels_fox_58267_1172675
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.PictureBox1.Image = My.Resources.fdba761ffc7cc3b85bd9bedffc295558
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.PictureBox1.Image = My.Resources.download
+    End Sub
+End Class
